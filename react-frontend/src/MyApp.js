127f09a900e080e8cdfcba2159d9ec63bd0220df
Priya Inampudi <inampudipriya121@gmail.com>
2023-10-03 10:05:00 -0700

    add frontend

---
+++ react-frontend/src/MyApp.js
@@ -0,0 +1,34 @@
+import React, {useState} from 'react';
+import Table from "./Table";
+import Form from './Form';
+
+
+function MyApp() {
+    //current state value: characters, update function: setCharacters
+    //const makes a tuple pair 
+    const [characters, setCharacters] = useState([]);
+
+      function removeOneCharacter (index) {
+	    const updated = characters.filter((character, i) => {
+	        return i !== index
+	    });
+	  setCharacters(updated);
+	}
+
+    function updateList(person) {
+        setCharacters([...characters, person]);
+      }
+
+    return (
+        <div className="container">
+            <Table characterData={characters} 
+                removeCharacter={removeOneCharacter} />
+            <Form handleSubmit={updateList} />
+        </div>  
+    )
+
+  }
+
+
+  export default MyApp; //makes component available to be
+  //imported into other comps
---
+++ react-frontend/src/Table.js
@@ -0,0 +1,55 @@
+import React from "react";
+
+
+//custom made table
+//naming convention for React components: capitalized, different from HTML elements
+
+
+function TableHeader() { //header of columns, name and job
+    return (
+      <thead>
+        <tr>
+          <th>Name</th>
+          <th>Job</th>
+        </tr>
+      </thead>
+    );
+  }
+ 
+  function TableBody(props) {
+    const rows = props.characterData.map((row, index) => {
+      return (
+        <tr key={index}>
+          <td>{row.name}</td>
+	      <td>{row.job}</td>
+	      <td>
+			        <button onClick={() => 
+				        props.removeCharacter(index)}>
+				        Delete
+			        </button>
+	      </td>
+        </tr>
+      );
+     }
+    );
+    return (
+        <tbody>
+          {rows}
+         </tbody>
+     );
+  }
+
+
+  function Table (props) {
+    return (
+      <table>
+        <TableHeader />
+        <TableBody characterData={props.characterData} 
+            removeCharacter={props.removeCharacter} />
+      </table>
+    );
+    //only passing one property characterData
+}
+
+
+export default Table;
