18e4b094f5f689561c4caa723293fe6aeeae7c08
Priya Inampudi <inampudipriya121@gmail.com>
2023-10-08 14:32:59 -0700

    post

--- packages/express-backend/backend.js
+++ packages/express-backend/backend.js
@@ -54,6 +54,30 @@ app.get('/users', (req, res) => {//endpoint to acceot http GET requests
         res.send(users);//processes request 
     }
 })
+const findUserById = (id) =>
+    users['users_list']
+        .find( (user) => user['id'] === id);
+    
+app.get('/users/:id', (req, res) => {
+    const id = req.params['id']; //or req.params.id
+    let result = findUserById(id);
+    if (result === undefined) {
+        res.status(404).send('Resource not found.');
+    } else {
+        res.send(result);
+    }
+})
+
+const addUser = (user) => {
+    users['users_list'].push(user);
+    return user;
+}
+
+app.post('/users', (req, res) => {
+    const userToAdd = req.body;
+    addUser(userToAdd);
+    res.send();
+})
 
 app.listen(port, () => { //has backend server listen to incoming requests on defined port
     console.log(`Example app listening at http://localhost:${port}`);
