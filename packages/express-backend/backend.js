c3e59053a5d066fe7e8097da86b6f699f9721b7f
Priya Inampudi <inampudipriya121@gmail.com>
2023-10-08 11:55:42 -0700

    add users

--- packages/express-backend/backend.js
+++ packages/express-backend/backend.js
@@ -6,10 +6,40 @@ const port = 8000; //localhost:8000
 
 app.use(express.json());
 
-app.get('/', (req, res) => { //endpoint to acceot http GET requests
-    res.send('Hello World!'); //processes request and response is a simple text
+const users = { 
+    users_list : [
+       { 
+          id : 'xyz789',
+          name : 'Charlie',
+          job: 'Janitor',
+       },
+       {
+          id : 'abc123', 
+          name: 'Mac',
+          job: 'Bouncer',
+       },
+       {
+          id : 'ppp222', 
+          name: 'Mac',
+          job: 'Professor',
+       }, 
+       {
+          id: 'yat999', 
+          name: 'Dee',
+          job: 'Aspring actress',
+       },
+       {
+          id: 'zap555', 
+          name: 'Dennis',
+          job: 'Bartender',
+       }
+    ]
+ }
+
+app.get('/users', (req, res) => { //endpoint to acceot http GET requests
+    res.send(users); //processes request and response is a simple text
 });
 
 app.listen(port, () => { //has backend server listen to incoming requests on defined port
-    console.log(`Example app listening at http://localhost:${port}`);
+    console.log(`Example app listening at http://localhost:${port}/users`);
 }); 
