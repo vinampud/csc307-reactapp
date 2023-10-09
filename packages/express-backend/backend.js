69b72c33bacbf2f42ac3d70ebcc48f6dc35c0930
Priya Inampudi <inampudipriya121@gmail.com>
2023-10-09 00:08:44 -0700

    backend part 1

--- packages/express-backend/backend.js
+++ packages/express-backend/backend.js
@@ -38,15 +38,30 @@ const users = {
 
  //?: prompts query 
  //query argument + = : searches for a match in that query argument
- const findUserByName = (name) => { 
-    return users['users_list']
-        .filter( (user) => user['name'] === name); //filtering based on argument
+ const findUserByName = (cur, name) => { 
+    return cur.filter( (user) => user['name'] === name); //filtering based on argument
+}
+
+const findUserByJob = (cur, job) => { 
+    return cur.filter( (user) => user['job'] === job); //filtering based on argument
 }
 
 app.get('/users', (req, res) => {//endpoint to acceot http GET requests
     const name = req.query.name;
-    if (name != undefined){
-        let result = findUserByName(name);
+    const job = req.query.job;
+    let result = users['users_list'];
+
+    //if either query is provided
+    if(name != undefined || job != undefined){
+
+        if (name != undefined){
+            result = findUserByName(result, name);
+        }
+
+        if (job != undefined){
+            result = findUserByJob(result, job);
+        }
+
         result = {users_list: result};
         res.send(result);
     }
@@ -54,6 +69,7 @@ app.get('/users', (req, res) => {//endpoint to acceot http GET requests
         res.send(users);//processes request 
     }
 })
+
 const findUserById = (id) =>
     users['users_list']
         .find( (user) => user['id'] === id);
@@ -77,6 +93,33 @@ app.post('/users', (req, res) => {
     const userToAdd = req.body;
     addUser(userToAdd);
     res.send();
+})
+
+
+// Helper function to delete a user by ID
+const deleteUserById = (id) => {
+    const userIndex = users['users_list'].findIndex((user) => user['id'] === id);
+
+    if (userIndex !== -1) {
+        // User found, remove the user from the array
+        users['users_list'].splice(userIndex, 1);
+        return true; // User deleted successfully
+    }
+    return false; // User not found
+}
+
+app.delete('/users/:id', (req, res) => {
+    const id = req.params.id; // Get the user ID from the URL parameter
+
+    const deleted = deleteUserById(id);
+
+    if (deleted) {
+        // User deleted successfully, respond with a 204 status code
+        res.status(204).send(); // 204 No Content
+    } else {
+        // User not found, respond with a 404 status code
+        res.status(404).send('User not found.');
+    }
 })
 
 app.listen(port, () => { //has backend server listen to incoming requests on defined port
